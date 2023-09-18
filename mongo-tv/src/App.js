7bdcd47105d393e9aba896c66361f5bfa5ffcaba
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-09-18 14:34:08 -0300

    Component peliculas ok

--- mongo-tv/src/App.js
+++ mongo-tv/src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Pelicula from "./Pelicula";
 
 function App() {
   return (
@@ -398,38 +399,19 @@ function App() {
                   <i className="ion-grid"></i>
                 </a>
               </div>
-              <div className="movie-item-style-2">
-                <img src="images/uploads/mv1.jpg" alt="" />
-                <div className="mv-item-infor">
-                  <h6>
-                    <a href="moviesingle.html">
-                      oblivion <span>(2012)</span>
-                    </a>
-                  </h6>
-                  <p className="rate">
-                    <i className="ion-android-star"></i>
-                    <span>8.1</span> /10
-                  </p>
-                  <p className="describe">
-                    Earth's mightiest heroes must come together and learn to
-                    fight as a team if they are to stop the mischievous Loki and
-                    his alien army from enslaving humanity...
-                  </p>
-                  <p className="run-time">
-                    {" "}
-                    Run Time: 2h21’ . <span>MMPA: PG-13 </span> .{" "}
-                    <span>Release: 1 May 2015</span>
-                  </p>
-                  <p>
-                    Director: <a href="#">Joss Whedon</a>
-                  </p>
-                  <p>
-                    Stars: <a href="#">Robert Downey Jr.,</a>{" "}
-                    <a href="#">Chris Evans,</a>{" "}
-                    <a href="#"> Chris Hemsworth</a>
-                  </p>
-                </div>
-              </div>
+              <Pelicula
+                titulo="oblivion (2012)"
+                calificacion="8.1"
+                director="Joss Whedon"
+                actores="Robert Downey Jr.,Chris Evans,
+              Chris Hemsworth"
+                fecha=" 1 May 2015"
+                duracion="2h 21min"
+              >
+                Earth's mightiest heroes must come together and learn to fight
+                as a team if they are to stop the mischievous Loki and his alien
+                army from enslaving humanity...
+              </Pelicula>
               <div className="movie-item-style-2">
                 <img src="images/uploads/mv2.jpg" alt="" />
                 <div className="mv-item-infor">

811832a636706908017f375a3476de5152cf72f2
Denis H. Roldán <112777347+denisrold@users.noreply.github.com>
2023-09-19 21:18:27 -0300

    Blog Redirect ok

--- mongo-tv/src/App.js
+++ mongo-tv/src/App.js
@@ -1,16 +1,22 @@
 import "./App.css";
 import Blog from "./Blog";
 import ListadoPeliculas from "./ListadoPeliculas";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
 function App() {
   return (
-    <Switch>
-      <Route path="/">
-        <ListadoPeliculas />
-      </Route>
-      <Route path="blog">
-        <Blog />
-      </Route>
-    </Switch>
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/blog">
+            <Blog />
+          </Route>
+
+          <Route path="/">
+            <ListadoPeliculas />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
   );
 }
 
